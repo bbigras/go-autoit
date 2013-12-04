864e916a7945d92e43d41886e5f0d13a8965ab2d
Bruno Bigras <bigras.bruno@gmail.com>
2013-12-04 10:06:44 -0500

    Implement Run()

---
+++ autoit.go
@@ -0,0 +1,25 @@
+package autoit
+
+/*
+#include <Windows.h>
+#include <AutoIt3.h>
+#cgo LDFLAGS: -lAutoItX3_x64
+*/
+import "C"
+
+import (
+	"syscall"
+)
+
+const (
+	SW_HIDE     = C.SW_HIDE     // Hidden window
+	SW_MINIMIZE = C.SW_MINIMIZE // Minimized window
+	SW_MAXIMIZE = C.SW_MAXIMIZE // Maximized window
+	SW_NORMAL   = 4
+)
+
+// Run a program and don't wait
+// Possibles flags are SW_HIDE, SW_MINIMIZE, SW_MAXIMIZE and SW_NORMAL
+func Run(filename, workingdir string, flag int) {
+	C.AU3_Run((*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(filename)), (*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(workingdir)), C.long(flag))
+}

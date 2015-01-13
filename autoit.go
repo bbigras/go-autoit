af1ebb6d1c2ebf6c7b23e605a17143e06d776985
Bruno Bigras <bigras.bruno@gmail.com>
2015-01-13 12:48:59 -0500

    golint

--- autoit.go
+++ autoit.go
@@ -38,24 +38,24 @@ import (
 )
 
 const (
-	SW_HIDE     = C.SW_HIDE     // Hidden window
-	SW_MINIMIZE = C.SW_MINIMIZE // Minimized window
-	SW_MAXIMIZE = C.SW_MAXIMIZE // Maximized window
-	SW_NORMAL   = 4
+	SwHide     = C.SW_HIDE     // Hidden window
+	SwMinimize = C.SW_MINIMIZE // Minimized window
+	SwMaximize = C.SW_MAXIMIZE // Maximized window
+	SwNormal   = 4
 )
 
 const (
-	ENABLE_USER_INPUT  = 0
-	DISABLE_USER_INPUT = 1
+	EnableUserInput  = 0
+	DisabelUserInput = 1
 )
 
 const (
-	STATE_EXISTS    = 1
-	STATE_VISIBLE   = 2
-	STATE_ENABLED   = 4
-	STATE_ACTIVE    = 8
-	STATE_MINIMIZED = 16
-	STATE_MAXIMIZED = 32
+	StateExists    = 1
+	StateVisible   = 2
+	StateEnabled   = 4
+	StateActive    = 8
+	StateMinimized = 16
+	StateMaximized = 32
 )
 
 // Run a program and don't wait
@@ -66,29 +66,33 @@ func Run(filename, workingdir string, flag int) (bool, int) {
 	return C.AU3_error() == 0, int(pid)
 }
 
-// Block the keyboard and mouse
+// BlockInput blocks the keyboard and mouse
 func BlockInput(flag int) {
 	C.AU3_BlockInput(C.long(flag))
 }
 
-// Close a window
+// WinClose closes a window
 func WinClose(title, text string) {
 	C.AU3_WinClose((*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(title)), (*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(text)))
 }
 
+// WinGetState returns a window's state
 func WinGetState(title, text string) (bool, int) {
 	result := C.AU3_WinGetState((*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(title)), (*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(text)))
 	return C.AU3_error() == 0, int(result)
 }
 
-func WinActive(title, text string) int {
-	return int(C.AU3_WinActive((*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(title)), (*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(text))))
+// WinActive returns true if the window is active
+func WinActive(title, text string) bool {
+	return int(C.AU3_WinActive((*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(title)), (*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(text)))) == 1
 }
 
-func WinExists(title, text string) int {
-	return int(C.AU3_WinExists((*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(title)), (*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(text))))
+// WinExists returns true if the window exist
+func WinExists(title, text string) bool {
+	return int(C.AU3_WinExists((*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(title)), (*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(text)))) == 1
 }
 
+// WinGetText returns the text contained in a window
 func WinGetText(title, text string, bufSize int) (result string) {
 	// TODO: test if bufSize is not greater than 64KB
 	if bufSize < 1 {
@@ -114,39 +118,40 @@ func WinGetText(title, text string, bufSize int) (result string) {
 	return
 }
 
+// WinActivate set the focus on a window
 func WinActivate(title, text string) {
 	C.AU3_WinActivate((*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(title)), (*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(text)))
 }
 
-// Simulate input on the keyboard
+// Send simulates input on the keyboard
 // flag: 0: normal, 1: raw
 func Send(keys string, flag int) {
 	C.AU3_Send((*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(keys)), C.long(flag))
 }
 
-// Get the color of the pixel at the specified location
+// PixelGetColor returns the color of the pixel at the specified location
 // return -1 if the location is invalid
 func PixelGetColor(x, y int) int {
 	return int(C.AU3_PixelGetColor(C.long(x), C.long(y)))
 }
 
-// Set/get a property
+// Opt is used to set/get a property
 func Opt(option string, param int) int {
 	return int(C.AU3_Opt((*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(option)), C.long(param)))
 }
 
-// click on a control without using the mouse pointer
+// ControlClick clicks on a control without using the mouse pointer
 // TODO: x, y should be center by defaut
 func ControlClick(title, text, controlID, button string, clicks, x, y int) int {
 	return int(C.AU3_ControlClick((*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(title)), (*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(text)), (*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(controlID)), (*_Ctype_WCHAR)(syscall.StringToUTF16Ptr(button)), C.long(clicks), C.long(x), C.long(y)))
 }
 
-// Get a checksum of the pixel in a region
+// PixelChecksum returns a checksum of the pixel in a region
 func PixelChecksum(left, top, right, bottom, step int) int64 {
 	return int64(C.AU3_PixelChecksum(C.long(left), C.long(top), C.long(right), C.long(bottom), C.long(step)))
 }
 
-// Move the mouse pointer to a specific location
+// MouseMove moves the mouse's pointer to a specific location
 func MouseMove(x, y, speed int) {
 	C.AU3_MouseMove(C.long(x), C.long(y), C.long(speed))
 }

cb8094cf658b7374d00d735c2c04a6d575c521d1
Bruno Bigras <bigras.bruno@gmail.com>
2016-10-13 14:50:23 -0400

    LockOSThread()

--- autoit.go
+++ autoit.go
@@ -29,6 +29,7 @@
 package autoit
 
 import (
+	"runtime"
 	"syscall"
 	"unicode/utf16"
 	"unsafe"
@@ -63,34 +64,56 @@ func (p utf16str) Swigcptr() uintptr {
 // Possibles flags are SwHide, SwMinimize, SwMaximize and SwNormal
 // returns true on success with the pid
 func Run(filename, workingdir string, flag int) (bool, int) {
-	pid := AU3_Run(utf16str(filename), utf16str(workingdir), flag)
-	return AU3_error() == 0, int(pid)
+	var pid int
+	var err bool
+	do(func() {
+		pid = AU3_Run(utf16str(filename), utf16str(workingdir), flag)
+		err = AU3_error() == 0
+	})
+	return err, pid
 }
 
 // WinClose closes a window
 func WinClose(title, text string) {
-	AU3_WinClose(utf16str(title), utf16str(text))
+	do(func() {
+		AU3_WinClose(utf16str(title), utf16str(text))
+	})
 }
 
 // WinGetState returns a window's state
 func WinGetState(title, text string) (bool, int) {
-	result := AU3_WinGetState(utf16str(title), utf16str(text))
-	return AU3_error() == 0, int(result)
+	var result int
+	var err bool
+	do(func() {
+		result = AU3_WinGetState(utf16str(title), utf16str(text))
+		err = AU3_error() == 0
+	})
+	return err, result
 }
 
 // WinSetState sets a window's state
 func WinSetState(title, text string, flag int) {
-	AU3_WinSetState(utf16str(title), utf16str(text), flag)
+	do(func() {
+		AU3_WinSetState(utf16str(title), utf16str(text), flag)
+	})
 }
 
 // WinActive returns true if the window is active
 func WinActive(title, text string) bool {
-	return int(AU3_WinActive(utf16str(title), utf16str(text))) == 1
+	var result bool
+	do(func() {
+		result = int(AU3_WinActive(utf16str(title), utf16str(text))) == 1
+	})
+	return result
 }
 
 // WinExists returns true if the window exist
 func WinExists(title, text string) bool {
-	return int(AU3_WinExists(utf16str(title), utf16str(text))) == 1
+	var result bool
+	do(func() {
+		result = int(AU3_WinExists(utf16str(title), utf16str(text))) == 1
+	})
+	return result
 }
 
 func findTermChr(buff []uint16) int {
@@ -104,45 +127,61 @@ func findTermChr(buff []uint16) int {
 
 // WinGetText returns the text contained in a window
 func WinGetText(title, text string, bufSize int) (result string) {
-	// TODO: test if bufSize is not greater than 64KB
-	if bufSize < 1 {
-		panic("bufSize must be greater than 0")
-	}
+	do(func() {
+		// TODO: test if bufSize is not greater than 64KB
+		if bufSize < 1 {
+			panic("bufSize must be greater than 0")
+		}
 
-	buff := make([]uint16, bufSize)
+		buff := make([]uint16, bufSize)
 
-	AU3_WinGetText(utf16str(title), utf16str(text), SwigcptrLPWSTR(unsafe.Pointer(&buff)), bufSize)
-	pos := findTermChr(buff)
+		AU3_WinGetText(utf16str(title), utf16str(text), SwigcptrLPWSTR(unsafe.Pointer(&buff)), bufSize)
+		pos := findTermChr(buff)
+		result = string(utf16.Decode(buff[0:pos]))
+	})
 
-	return string(utf16.Decode(buff[0:pos]))
+	return result
 }
 
 // WinActivate set the focus on a window
 func WinActivate(title, text string) {
-	AU3_WinActivate(utf16str(title), utf16str(text))
+	do(func() {
+		AU3_WinActivate(utf16str(title), utf16str(text))
+	})
 }
 
 // Send simulates input on the keyboard
 // flag: 0: normal, 1: raw
 func Send(keys string, flag int) {
-	AU3_Send(utf16str(keys), flag)
+	do(func() {
+		AU3_Send(utf16str(keys), flag)
+	})
 }
 
 // PixelGetColor returns the color of the pixel at the specified location
 // return -1 if the location is invalid
-func PixelGetColor(x, y int) int {
-	return int(AU3_PixelGetColor(x, y))
+func PixelGetColor(x, y int) (result int) {
+	do(func() {
+		result = int(AU3_PixelGetColor(x, y))
+	})
+	return result
 }
 
 // Opt is used to set/get a property
-func Opt(option string, param int) int {
-	return int(AU3_Opt(utf16str(option), param))
+func Opt(option string, param int) (result int) {
+	do(func() {
+		result = int(AU3_Opt(utf16str(option), param))
+	})
+	return result
 }
 
 // ControlClick clicks on a control without using the mouse pointer
 // TODO: x, y should be center by defaut
-func ControlClick(title, text, controlID, button string, clicks, x, y int) int {
-	return int(AU3_ControlClick(utf16str(title), utf16str(text), utf16str(controlID), utf16str(button), clicks, x, y))
+func ControlClick(title, text, controlID, button string, clicks, x, y int) (result int) {
+	do(func() {
+		result = int(AU3_ControlClick(utf16str(title), utf16str(text), utf16str(controlID), utf16str(button), clicks, x, y))
+	})
+	return result
 }
 
 // https://msdn.microsoft.com/en-us/library/windows/desktop/dd162897(v=vs.85).aspx
@@ -151,12 +190,45 @@ type rect struct {
 }
 
 // PixelChecksum returns a checksum of the pixel in a region
-func PixelChecksum(left, top, right, bottom int32, step int) int64 {
-	r := &rect{left, top, right, bottom}
-	return int64(AU3_PixelChecksum(SwigcptrLPRECT(unsafe.Pointer(&r)), step))
+func PixelChecksum(left, top, right, bottom int32, step int) (result int64) {
+	do(func() {
+		r := &rect{left, top, right, bottom}
+		result = int64(AU3_PixelChecksum(SwigcptrLPRECT(unsafe.Pointer(&r)), step))
+	})
+	return result
 }
 
 // MouseMove moves the mouse's pointer to a specific location
 func MouseMove(x, y, speed int) {
-	AU3_MouseMove(x, y, speed)
+	do(func() {
+		AU3_MouseMove(x, y, speed)
+	})
+}
+
+// Arrange that main.main runs on main thread.
+func init() {
+	runtime.LockOSThread()
+}
+
+// Main runs the main AutoIt service loop.
+// The binary's main.main must call autoit.Main() to run this loop.
+// Main does not return. If the binary needs to do other work, it
+// must do it in separate goroutines.
+func Main() {
+	for f := range mainfunc {
+		f()
+	}
+}
+
+// queue of work to run in main thread.
+var mainfunc = make(chan func())
+
+// do runs f on the main thread.
+func do(f func()) {
+	done := make(chan bool, 1)
+	mainfunc <- func() {
+		f()
+		done <- true
+	}
+	<-done
 }
